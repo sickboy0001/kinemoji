0c5db1b8926ee96ac494a3c72ef48c9ca0bdb681
sickboy0001 <syunjyu0001@gmail.com>
2026-04-13 07:02:38 +0900

    fx aync func

--- src/app/api/kinemoji/status/[id]/route.ts
+++ src/app/api/kinemoji/status/[id]/route.ts
@@ -11,7 +11,7 @@ const EXTERNAL_API_URL =
  */
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
 
